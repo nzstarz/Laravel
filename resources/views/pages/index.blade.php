a59cc5fc688ebd9418543042f537639ac0823f3f
Nazmul M Hasan <30605297+nzstarz@users.noreply.github.com>
2021-06-21 14:14:20 +0600

    Basic Routing & Controllers

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'LSAPP')}}</title>
+
+         
+       
+    </head>
+   <body>
+        <h1> Welcome to Laravel Practice Session</h1>
+        <p> This is the Laravel 8 Primary Practice Classes</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/users/{id}/{name}', function($id, $name){
+    return 'This is User ' .$name. ' with an id of ' .$id;
+});
+
+Route::get('/about', function(){
+    return view('pages.about');
+});
+*/
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
