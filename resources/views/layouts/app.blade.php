d95b9306c90b40043ac34a29fd33595146d23b4c
nzstarz <nazmul.hasan@northsouth.edu>
2021-06-27 01:05:48 +0600

    Blade Templating & Compiling Assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,20 +3,30 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script type="text/javascript" src="{{asset('js/app.js') }}"></script>
+         
+       
+      <!--  <link rel ="stylesheet" herf="{{'asset/css/app.css'}}"> -->
+        
+       <!--  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">, none of above two worked
+        
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">-->
+        
+        <!--<script type="text/javascript" src="{{asset('js/app.js') }}"></script>-->
+         <link href="{{asset('css/app.css') }}" rel="stylesheet">
+         
 
-         <link rel ="stylesheet" herf="{{'asset/css/app.css'}}">  
         
         
-        <title>{{config('app.name', 'LSAPP')}}</title>
+            <title>{{config('app.name', 'LSAPP')}}</title>
 
          
        
     </head>
    <body>
-        <div class="container">
+       @include('inc.navbar')
+         <div class="container">
             @yield('content')
         </div>
-    
+        <script src="{{asset('js/app.js')}}"></script>
     </body>
     </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,15 @@
 
 @section('content')
     
-        <h1> {{$title}}</h1>
-        <p> This is the Laravel 8 Primary Practice Classes</p>
+       <!-- <h1> {{$title}}</h1>
+        <p> This is the Laravel 8 Primary Practice Classes</p>-->
+
+        <div class="jumbotron text-center">
+  <h1 class="display-4">Welcome to Laravel !</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-primary btn-lg" href="/register" role="button">register</a>
+</div>
   
 @endsection
