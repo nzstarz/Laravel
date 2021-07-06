6d8b244ec0c75031191f4a8945ce66d20fc829ee
nzstarz <nazmul.hasan@northsouth.edu>
2021-07-06 23:37:34 +0600

    Access Control

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,13 +10,17 @@
     <hr>
         <small> Written On {{$post->created_at}} by {{$post->user->name}}</small>
     </hr>
-   <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+        <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
 
-   {!!Form::open(['action'=>['PostController@destroy', $post->id], 'method'=>'POST', 'class' =>'float-right'])!!}
-    {{Form::hidden('_method', 'DELETE')}}
-    {{Form::submit('Delete', ['class'=>'btn btn-danger'])}}
+            {!!Form::open(['action'=>['PostController@destroy', $post->id], 'method'=>'POST', 'class' =>'float-right'])!!}
+                {{Form::hidden('_method', 'DELETE')}}
+                {{Form::submit('Delete', ['class'=>'btn btn-danger'])}}
 
-    {!!Form::close()!!}
+            {!!Form::close()!!}
+        @endif
+    @endif
    <!--  <a href="{!!url('/posts/{{$post->id}}/edit'); !!}" class="btn btn-primary">Edit</a>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-warning" onclick="return confirm('are you sure?')">Edit</a>-->
 @endsection
